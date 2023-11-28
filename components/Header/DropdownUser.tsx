9f9d9716c63a1db37e5e30d891486651fee566b1
Qodestackr <winchygichu@gmail.com>
2023-11-28 15:45:10 +0300

    update logout

--- components/Header/DropdownUser.tsx
+++ components/Header/DropdownUser.tsx
@@ -3,10 +3,11 @@
 import { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const DropdownUser = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const router = useRouter();
 
   const trigger = useRef<any>(null);
   const dropdown = useRef<any>(null);
@@ -14,7 +15,7 @@ const DropdownUser = () => {
   // const router = useRouter();
 
   const handleLogout = (e: any) => {
-    // router?. NAVIGATE TO "/"
+    router.push('/');
   };
 
   // close on click outside
@@ -166,7 +167,9 @@ const DropdownUser = () => {
             </Link>
           </li>
         </ul>
-        <button className="flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+        <button 
+        onClick={handleLogout}
+        className="flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
           <svg
             className="fill-current"
             width="22"
