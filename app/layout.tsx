0220b3c7e3f6917d03f2ccf475b0c7a317b25681
sonylomo <sonylomo1@gmail.com>
2024-02-21 16:30:17 +0300

    feat: training program list

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
             <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
               <Headerzz />
               <main>
-                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                <div className="p-4 md:p-0">
                   {children}
                   <Footer />
                 </div>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  // devServer: { fastRefresh: true },
   images: {
     domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com']
-  },
+  }
   //useFileSystemPublicRoutes: false,
 };
 
